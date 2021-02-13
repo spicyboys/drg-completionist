f9ed3443c7f9980a0182e4b718260ef0ddf37530
Robert Lynch <robertlynch2012@gmail.com>
2021-02-13 17:35:43 -0600

    Add basic structure to Overclocks

--- src/pages/overclocks/Overclocks.tsx
+++ src/pages/overclocks/Overclocks.tsx
@@ -1,5 +1,18 @@
 import { Card } from "antd";
 
 export default function Overclocks() {
-  return <Card title="Overclocks">Inner Card content</Card>;
+  return (
+    <Card>
+      <Card title="Driller">Inner Card content</Card>
+      <Card style={{ marginTop: 16 }} title="Engineer">
+        Inner Card content
+      </Card>
+      <Card style={{ marginTop: 16 }} title="Gunner">
+        Inner Card content
+      </Card>
+      <Card style={{ marginTop: 16 }} title="Scout">
+        Inner Card content
+      </Card>
+    </Card>
+  );
 }
