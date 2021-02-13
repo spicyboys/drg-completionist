4be1d1cda8eba8a0f05c237735a8c4e6c1bcef56
Robert Lynch <robertlynch2012@gmail.com>
2021-02-13 17:25:19 -0600

    Add more structure around tabs and add basic Overclock layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,69 @@
 import { Row, Col, PageHeader, Tabs } from "antd";
 import "antd/dist/antd.dark.css";
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory, useLocation, Switch, Route } from "react-router-dom";
+import Overclocks from "pages/overclocks/Overclocks";
 
 const { TabPane } = Tabs;
+
+type TabName = "overclocks" | "frameworks" | "skins" | "accessories" | "pickax";
+
+const DEFAULT_TAB: TabName = "overclocks";
+
+const TABS: Array<{ title: string; key: TabName; content: JSX.Element }> = [
+  {
+    title: "Overclocks",
+    key: "overclocks",
+    content: <Overclocks />,
+  },
+  {
+    title: "Weapon frameworks",
+    key: "frameworks",
+    content: <></>,
+  },
+  {
+    title: "Weapon skins",
+    key: "skins",
+    content: <></>,
+  },
+  {
+    title: "Miner Accessories",
+    key: "accessories",
+    content: <></>,
+  },
+  {
+    title: "Pickax Components",
+    key: "pickax",
+    content: <></>,
+  },
+];
 
 export default function App() {
   const history = useHistory();
   const location = useLocation();
   return (
-    <Row gutter={[16, 16]}>
-      <Col span={18} offset={3} push={3}>
+    <Row gutter={16}>
+      <Col span={18} offset={3}>
         <PageHeader
           title="DRG Completionist"
           subTitle="Rock and Stone!"
           footer={
             <Tabs
-              activeKey={location.pathname.substring(1) || "overclocks"}
+              activeKey={location.pathname.substring(1) || DEFAULT_TAB}
               onChange={history.push}
             >
-              <TabPane tab="Overclocks" key="overclocks" />
-              <TabPane tab="Weapon frameworks" key="frameworks" />
-              <TabPane tab="Weapon skins" key="skins" />
-              <TabPane tab="Miner Accessories" key="accessories" />
-              <TabPane tab="Pickax Components" key="pickax" />
+              {TABS.map((tab) => (
+                <TabPane tab={tab.title} key={tab.key} />
+              ))}
             </Tabs>
           }
         />
+        <Switch>
+          {TABS.map((tab) => (
+            <Route exact path={`/${tab.key}`} key={tab.key}>
+              {tab.content}
+            </Route>
+          ))}
+        </Switch>
       </Col>
     </Row>
   );
