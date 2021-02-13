513723e2d44c9151169e12fb3fd58387a1be0037
Robert Lynch <robertlynch2012@gmail.com>
2021-02-13 15:54:56 -0600

    Clean up imports in antd skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import {
 } from "@ant-design/icons";
 import "antd/dist/antd.dark.css";
 
-const { SubMenu } = Menu;
+const { SubMenu, Item: MenuItem } = Menu;
 const { Header, Content, Sider } = Layout;
+const { Item: BreadcrumbItem } = Breadcrumb;
 
 export default function App() {
   return (
@@ -15,9 +16,9 @@ export default function App() {
       <Header className="header">
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
+          <MenuItem key="1">nav 1</MenuItem>
+          <MenuItem key="2">nav 2</MenuItem>
+          <MenuItem key="3">nav 3</MenuItem>
         </Menu>
       </Header>
       <Layout>
@@ -29,34 +30,34 @@ export default function App() {
             style={{ height: "100%", borderRight: 0 }}
           >
             <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-              <Menu.Item key="1">option1</Menu.Item>
-              <Menu.Item key="2">option2</Menu.Item>
-              <Menu.Item key="3">option3</Menu.Item>
-              <Menu.Item key="4">option4</Menu.Item>
+              <MenuItem key="1">option1</MenuItem>
+              <MenuItem key="2">option2</MenuItem>
+              <MenuItem key="3">option3</MenuItem>
+              <MenuItem key="4">option4</MenuItem>
             </SubMenu>
             <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-              <Menu.Item key="5">option5</Menu.Item>
-              <Menu.Item key="6">option6</Menu.Item>
-              <Menu.Item key="7">option7</Menu.Item>
-              <Menu.Item key="8">option8</Menu.Item>
+              <MenuItem key="5">option5</MenuItem>
+              <MenuItem key="6">option6</MenuItem>
+              <MenuItem key="7">option7</MenuItem>
+              <MenuItem key="8">option8</MenuItem>
             </SubMenu>
             <SubMenu
               key="sub3"
               icon={<NotificationOutlined />}
               title="subnav 3"
             >
-              <Menu.Item key="9">option9</Menu.Item>
-              <Menu.Item key="10">option10</Menu.Item>
-              <Menu.Item key="11">option11</Menu.Item>
-              <Menu.Item key="12">option12</Menu.Item>
+              <MenuItem key="9">option9</MenuItem>
+              <MenuItem key="10">option10</MenuItem>
+              <MenuItem key="11">option11</MenuItem>
+              <MenuItem key="12">option12</MenuItem>
             </SubMenu>
           </Menu>
         </Sider>
         <Layout style={{ padding: "0 24px 24px" }}>
           <Breadcrumb style={{ margin: "16px 0" }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
+            <BreadcrumbItem>Home</BreadcrumbItem>
+            <BreadcrumbItem>List</BreadcrumbItem>
+            <BreadcrumbItem>App</BreadcrumbItem>
           </Breadcrumb>
           <Content
             className="site-layout-background"
