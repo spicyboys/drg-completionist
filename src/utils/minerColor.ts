37832c86dc50a31daf4542e3243181abf5c6d327
Robert Lynch <robertlynch2012@gmail.com>
2021-02-14 12:31:32 -0600

    Add utilities for various miner types and values

---
+++ src/utils/minerColor.ts
@@ -0,0 +1,10 @@
+import Miner from "types/miner";
+
+const MinerColor: Record<Miner, string> = {
+  [Miner.Driller]: "#f7fe00",
+  [Miner.Engineer]: "#ed1d0a",
+  [Miner.Gunner]: "#3d7029",
+  [Miner.Scout]: "#4351ff",
+};
+
+export default MinerColor;
