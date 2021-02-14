9d2f262b5f93062e9f2f7706928a373530d48f08
Robert Lynch <robertlynch2012@gmail.com>
2021-02-14 15:14:23 -0600

    Add framework data to state

--- src/data/state.ts
+++ src/data/state.ts
@@ -1,7 +1,10 @@
+import { Framework } from "pages/frameworks/FrameworkData";
 import Miner from "types/miner";
+import MinerWeapon from "types/minerWeapons";
 
 export type State = {
   overclocks: Record<Miner, string[]>;
+  frameworks: { [M in Miner]: { [W in MinerWeapon[M]]: Framework[] } };
 };
 
 export const INITIAL_STATE: State = {
@@ -10,5 +13,31 @@ export const INITIAL_STATE: State = {
     [Miner.Engineer]: [],
     [Miner.Gunner]: [],
     [Miner.Scout]: [],
+  },
+  frameworks: {
+    [Miner.Driller]: {
+      "CRISPR Flamethrower": [],
+      "Cryo Cannon": [],
+      "Subata 120": [],
+      "Experimental Plasma Charger": [],
+    },
+    [Miner.Engineer]: {
+      '"Warthog" Auto 210': [],
+      '"Stubby" Voltaic SMG': [],
+      "Deepcore 40mm PGL": [],
+      "Breach Cutter": [],
+    },
+    [Miner.Gunner]: {
+      '"Lead Storm" Powered Minigun': [],
+      '"Thunderhead" Heavy Autocannon': [],
+      '"Bulldog" Heavy Revolver': [],
+      "BRT7 Burst Fire Gun": [],
+    },
+    [Miner.Scout]: {
+      "Deepcore GK2": [],
+      "M1000 Classic": [],
+      "Jury-Rigged Boomstick": [],
+      "Zhukov NUK17": [],
+    },
   },
 };
---
+++ src/pages/frameworks/FrameworkData.ts
@@ -0,0 +1,6 @@
+export type Framework =
+  | "Custom Engineered"
+  | "Fourth Relic"
+  | "Glyphid Trophy Hunter"
+  | "Mechanized"
+  | "Neonband";
