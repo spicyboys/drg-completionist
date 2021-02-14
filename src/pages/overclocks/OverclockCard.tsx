e5f9d965c6f2716a3e9ccb81971d1679fb384ea9
Fleckeri <16787403+Fleckeri@users.noreply.github.com>
2021-02-14 09:05:03 -0600

    Make first pass at an Overclock detail hover

--- src/pages/overclocks/OverclockCard.tsx
+++ src/pages/overclocks/OverclockCard.tsx
@@ -1,34 +1,82 @@
 import OverclockIcon from "./OverclockIcon";
 import { Overclock } from "./OverclockData";
 import { ClassColors } from "utils/classColors";
-import { Card, Col } from "antd";
+import { Card, Col, Divider, Popover, Row, Typography } from "antd";
+
+const { Paragraph, Text } = Typography;
 
 export default function OverclockCard(props: {
   overclock: Overclock;
   isAcquired: boolean;
   onClick: () => void;
 }) {
+
+  const detailedPopover = (
+    <Card
+      hoverable
+      title={props.overclock.name}
+      headStyle={
+        props.isAcquired
+          ? {
+            color: "black",
+            backgroundColor: ClassColors[props.overclock.class],
+            fontWeight: "bold",
+            transition: "all .2s",
+          }
+          : {
+            fontWeight: "bold",
+            transition: "all .2s",
+          }
+      }
+      style={{ width: 400 }}
+      onClick={props.onClick}
+    >
+      <OverclockIcon overclock={props.overclock} />
+      <Divider dashed />
+      {props.overclock.effects.buffs.map((buff) => (
+        <Row justify={"center"}>
+          <Col>
+            <Text strong type={"success"}>{buff}</Text>
+          </Col>
+        </Row>
+      ))}
+      {props.overclock.effects.nerfs.map((nerf) => (
+        <Row justify={"center"}>
+          <Col>
+            <Text strong type={"danger"}>{nerf}</Text>
+          </Col>
+        </Row>
+      ))}
+      <Divider dashed />
+      <Paragraph>{props.overclock.description}</Paragraph>
+    </Card>
+  );
+
   return (
-    <Col span={3} key={props.overclock.name}>
-      <Card
-        hoverable
-        title={props.overclock.name}
-        headStyle={
-          props.isAcquired
-            ? {
-              color: "black",
-              backgroundColor: ClassColors[props.overclock.class],
-              fontWeight: "bold",
-              transition: "all .2s",
-            }
-            : {
-              transition: "all .2s",
-            }
-        }
-        onClick={props.onClick}
-      >
-        <OverclockIcon overclock={props.overclock} />
-      </Card>
-    </Col>
+    <Popover content={detailedPopover}>
+      <Col span={3} key={props.overclock.name}>
+        <Card
+          hoverable
+          title={props.overclock.name}
+          headStyle={
+            props.isAcquired
+              ? {
+                color: "black",
+                backgroundColor: ClassColors[props.overclock.class],
+                fontWeight: "bold",
+                transition: "all .2s",
+              }
+              : {
+                fontWeight: "bold",
+                transition: "all .2s",
+              }
+          }
+          onClick={props.onClick}
+        >
+          <OverclockIcon overclock={props.overclock} />
+        </Card>
+      </Col>
+    </Popover>
+
   );
 }
