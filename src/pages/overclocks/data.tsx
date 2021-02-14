45b582bb105fa58f87b3328baeebb461c80c8340
Nich <kanosea@gmail.com>
2021-02-13 19:14:00 -0600

    Streamline Overclock types

--- src/pages/overclocks/data.tsx
+++ src/pages/overclocks/data.tsx
@@ -20,21 +20,49 @@ type Scout = {
   weapon: "Assualt Rifle" | "M1000" | "Boomstick" | "Zhukovs";
 };
 
-export type Overclock = {
+type Price = {
+  credits: number,
+  bismor?: number,
+  croppa?: number,
+  enorPearl?: number,
+  jadiz?: number,
+  magnite?: number,
+  umanite?: number,
+}
+
+export type Overclock<T extends (Driller | Engineer | Gunner | Scout)> = {
   name: string;
   type: "Clean" | "Balanced" | "Unstable";
   icon: keyof typeof icons;
-  price?: string[];
+  price: Price,
   effects?: string[];
   description?: string;
-} & (Driller | Engineer | Gunner | Scout);
+} & T;
 
-export const overclocks: Overclock[] = [
-  {
-    name: "Lighter Tanks",
-    class: "Driller",
-    icon: "area",
-    weapon: "Flamethrower",
-    type: "Clean",
-  },
-];
+type Overclocks = {
+  driller: Overclock<Driller>[],
+  engineer: Overclock<Engineer>[],
+  gunner: Overclock<Gunner>[],
+  scout: Overclock<Scout>[],
+}
+
+export const overclocks: Overclocks = {
+  driller: [
+    {
+      name: "Lighter Tanks",
+      class: "Driller",
+      icon: "area",
+      weapon: "Flamethrower",
+      type: "Clean",
+      price: {
+        credits: 7500,
+        bismor: 125,
+        croppa: 75,
+        umanite: 90,
+      }
+    }
+  ],
+  engineer: [],
+  gunner: [],
+  scout: []
+};
