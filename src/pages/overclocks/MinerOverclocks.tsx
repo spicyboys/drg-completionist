1971fbada8e15592428175bba1f5cb8bad84873d
Robert Lynch <robertlynch2012@gmail.com>
2021-02-14 09:32:33 -0600

    Make panels expandable

--- src/pages/overclocks/MinerOverclocks.tsx
+++ src/pages/overclocks/MinerOverclocks.tsx
@@ -2,61 +2,66 @@ import { Card, Row, Image, Progress, Divider } from "antd";
 import { Overclock } from "./OverclockData";
 import OverclockCard from "./OverclockCard";
 import { useMemo, useState } from "react";
+import { Collapse, CollapsePanelProps } from "antd";
+import React from "react";
 
+const { Panel } = Collapse;
 const { Meta } = Card;
 
-export default function MinerOverclocks(props: {
-  title: string;
-  img: string;
-  overclocks: Overclock[];
-  classColor: string;
-  style?: React.CSSProperties;
-}) {
+export default function MinerOverclocks(
+  props: {
+    title: string;
+    img: string;
+    overclocks: Overclock[];
+    classColor: string;
+  } & Omit<CollapsePanelProps, "key" | "header">
+) {
+  const { title, img, overclocks, classColor, ...panelProps } = props;
   const [acquiredOverclocks, setAcquiredOverclocks] = useState<Array<string>>(
     []
   );
 
   const groupedOverclocked = useMemo(() => {
     const g: { [k: string]: Overclock[] } = {};
-    for (const overclock of props.overclocks) {
+    for (const overclock of overclocks) {
       if (g[overclock.weapon] === undefined) {
         g[overclock.weapon] = [];
       }
       g[overclock.weapon].push(overclock);
     }
     return g;
-  }, [props.overclocks]);
+  }, [overclocks]);
 
   return (
-    <Card
-      type="inner"
-      title={
+    <Panel
+      {...panelProps}
+      style={{ marginTop: 16 }}
+      header={
         <Meta
-          title={props.title}
-          avatar={
-            <Image src={props.img} preview={false} height={64} width={64} />
-          }
+          title={title}
+          avatar={<Image src={img} preview={false} height={64} width={64} />}
           description={
             <Progress
               percent={Math.round(
-                (acquiredOverclocks.length / props.overclocks.length) * 100
+                (acquiredOverclocks.length / overclocks.length) * 100
               )}
               strokeColor={{
-                '0%': props.classColor,
-                '100%': '#87d068',
+                "0%": classColor,
+                "100%": "#87d068",
               }}
             />
           }
         />
       }
-      style={props.style}
+      key={title}
     >
       {Object.entries(groupedOverclocked).map(([weapon, overclocks]) => (
-        <>
+        <React.Fragment key={weapon}>
           <Divider orientation="left">{weapon}</Divider>
           <Row gutter={[16, 16]}>
             {overclocks.map((overclock) => (
               <OverclockCard
+                key={overclock.name}
                 overclock={overclock}
                 isAcquired={acquiredOverclocks.includes(overclock.name)}
                 onClick={() => {
@@ -74,8 +79,8 @@ export default function MinerOverclocks(props: {
               />
             ))}
           </Row>
-        </>
+        </React.Fragment>
       ))}
-    </Card>
+    </Panel>
   );
 }
