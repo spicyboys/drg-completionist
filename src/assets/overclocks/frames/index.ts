83841c1c8d6c05c894863dd4c46c697f67fddd54
Robert Lynch <robertlynch2012@gmail.com>
2021-02-13 22:17:33 -0600

    Update import paths for brevity

--- src/assets/overclocks/frames/index.ts
+++ src/assets/overclocks/frames/index.ts
@@ -3,7 +3,7 @@ const frames = <M extends { [k: string]: Promise<typeof import("*.png")> }>(
 ) => things;
 
 export default frames({
-  clean: import("assets/overclocks/frames/clean.png"),
-  balanced: import("assets/overclocks/frames/balanced.png"),
-  unstable: import("assets/overclocks/frames/unstable.png"),
+  clean: import("./clean.png"),
+  balanced: import("./balanced.png"),
+  unstable: import("./unstable.png"),
 });
