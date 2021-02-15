e7c350d62b8a1f65b04a0b119abb210aa2a22b3e
Robert Lynch <robertlynch2012@gmail.com>
2021-02-15 12:38:36 -0600

    Simplify reducer slightly

--- src/data/reducer.ts
+++ src/data/reducer.ts
@@ -5,55 +5,36 @@ export default function reducer(state: State, action: Actions): State {
   switch (action.type) {
     case "TOGGLE_OVERCLOCK": {
       const { miner, overclock } = action.payload;
-      if (state.overclocks[miner].includes(overclock)) {
-        return {
-          ...state,
-          overclocks: {
-            ...state.overclocks,
-            [miner]: state.overclocks[miner].filter((v) => v !== overclock),
-          },
-        };
-      } else {
-        return {
-          ...state,
-          overclocks: {
-            ...state.overclocks,
-            [miner]: [...state.overclocks[miner], overclock],
-          },
-        };
-      }
+      return {
+        ...state,
+        overclocks: {
+          ...state.overclocks,
+          [miner]: state.overclocks[miner].includes(overclock)
+            ? state.overclocks[miner].filter((v) => v !== overclock)
+            : [...state.overclocks[miner], overclock],
+        },
+      };
     }
     case "TOGGLE_FRAMEWORK": {
       const { miner, weapon, framework } = action.payload;
-      // @ts-ignore
-      if (state.frameworks[miner][weapon].includes(framework)) {
-        return {
-          ...state,
-          frameworks: {
-            ...state.frameworks,
-            [miner]: {
-              ...state.frameworks[miner],
-              // @ts-ignore
-              [weapon]: state.frameworks[miner][weapon].filter(
-                // @ts-ignore
-                (v) => v !== framework
-              ),
-            },
+      return {
+        ...state,
+        frameworks: {
+          ...state.frameworks,
+          [miner]: {
+            ...state.frameworks[miner],
+            // @ts-ignore
+            [weapon]: state.frameworks[miner][weapon].includes(framework)
+              ? // @ts-ignore
+                state.frameworks[miner][weapon].filter(
+                  // @ts-ignore
+                  (v) => v !== framework
+                )
+              : // @ts-ignore
+                [...state.frameworks[miner][weapon], framework],
           },
-        };
-      } else {
-        return {
-          ...state,
-          frameworks: {
-            ...state.frameworks,
-            [miner]: {
-              ...state.frameworks[miner],
-              // @ts-ignore
-              [weapon]: [...state.frameworks[miner][weapon], framework],
-            },
-          },
-        };
-      }
+        },
+      };
     }
     default:
       return state;
