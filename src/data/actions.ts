8b2d262671b439341acaa87c963d93570ce1f5ec
Robert Lynch <robertlynch2012@gmail.com>
2021-02-15 00:58:44 -0600

    add in like a ton of shit for frameworks

--- src/data/actions.ts
+++ src/data/actions.ts
@@ -1,11 +1,22 @@
-import { State } from "./state";
+import Miner from "types/miner";
+import { Framework } from "pages/frameworks/FrameworkData";
+import { MinerWeapon } from "utils/minerWeapons";
 
-type Actions = {
-  type: "TOGGLE_OVERCLOCK";
-  payload: {
-    miner: keyof State["overclocks"];
-    overclock: string;
-  };
-};
+type Actions =
+  | {
+      type: "TOGGLE_OVERCLOCK";
+      payload: {
+        miner: Miner;
+        overclock: string;
+      };
+    }
+  | {
+      type: "TOGGLE_FRAMEWORK";
+      payload: {
+        miner: Miner;
+        weapon: MinerWeapon<Miner>;
+        framework: Framework;
+      };
+    };
 
 export default Actions;
--- src/pages/frameworks/MinerFrameworks.tsx
+++ src/pages/frameworks/MinerFrameworks.tsx
@@ -1,20 +1,47 @@
+// @ts-nocheck
+
 import { Row, Divider } from "antd";
 import React from "react";
 import Miner from "types/miner";
 import { Frameworks } from "./FrameworkData";
-import { MinerWeapons } from "utils/minerWeapons";
+import { MinerWeapons, MinerWeapon } from "utils/minerWeapons";
+import useStore from "data/useStore";
+import FrameworkCard from "./FrameworkCard";
 
-export default function MinerOverclocks(props: { miner: Miner }) {
+/**
+ * This whole file is a trainwreck for Typescript. Technically, everything in
+ * here should be well typed, but the path traversal for objects with enum
+ * keys doesn't work. And because of that, well, nothing else types correctly.
+ *
+ * I spent all afternoon trying to fix it. Instead, you're going to have to
+ * deal with this ts-nochecked'd dogshit
+ */
+export default function MinerFrameworks<T extends Miner>(props: { miner: T }) {
   const { miner } = props;
+  const [acquiredMinerFrameworks, dispatch] = useStore("frameworks", miner);
 
   return (
     <>
-      {(MinerWeapons[miner] as readonly string[]).map((weapon) => (
+      {(MinerWeapons[miner] as readonly MinerWeapon<T>[]).map((weapon) => (
         <React.Fragment key={weapon}>
           <Divider orientation="left">{weapon}</Divider>
           <Row gutter={[16, 16]}>
-            {Frameworks.map((overclock) => (
-              <h1>{overclock}</h1>
+            {Frameworks.map((framework) => (
+              <FrameworkCard
+                miner={miner}
+                framework={framework}
+                isAcquired={acquiredMinerFrameworks[weapon].includes(framework)}
+                onClick={() =>
+                  dispatch({
+                    type: "TOGGLE_FRAMEWORK",
+                    payload: {
+                      miner,
+                      weapon,
+                      framework,
+                    },
+                  })
+                }
+              />
             ))}
           </Row>
         </React.Fragment>
