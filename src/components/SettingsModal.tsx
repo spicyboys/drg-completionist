9868ef46017d81420ba90e56be75616384febcaa
Robert Lynch <robertlynch2012@gmail.com>
2021-02-15 14:28:38 -0600

    Add github and sellout links

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,5 +1,7 @@
-import { Modal, Button, Typography, Space } from "antd";
+import { Modal, Button, Typography, Space, Divider } from "antd";
 import useStore from "data/useStore";
+import GitHubButton from "react-github-btn";
+
 const { Title, Text } = Typography;
 
 export default function SettingsModal(props: {
@@ -25,6 +27,46 @@ export default function SettingsModal(props: {
           Reset
         </Button>
       </Space>
+      <Divider />
+      <Space>
+        <GitHubButton
+          href="https://github.com/BobertForever/drg-completionist"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Star BobertForever/drg-completionist on GitHub"
+        >
+          Star
+        </GitHubButton>
+        <GitHubButton
+          href="https://github.com/BobertForever/drg-completionist/fork"
+          data-color-scheme="no-preference: light; light: light; dark: light;"
+          data-icon="octicon-repo-forked"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Fork BobertForever/drg-completionist on GitHub"
+        >
+          Fork
+        </GitHubButton>
+        <GitHubButton
+          href="https://github.com/BobertForever/drg-completionist/issues"
+          data-color-scheme="no-preference: light; light: light; dark: light;"
+          data-icon="octicon-issue-opened"
+          data-size="large"
+          data-show-count="true"
+          aria-label="Issue BobertForever/drg-completionist on GitHub"
+        >
+          Issue
+        </GitHubButton>
+      </Space>
+      <Divider />
+      <div style={{ textAlign: "center" }}>
+        <a href="https://www.buymeacoffee.com/robort">
+          <img
+            src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=robort&button_colour=5F7FFF&font_colour=ffffff&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00"
+            alt="Buy me a coffee"
+          />
+        </a>
+      </div>
     </Modal>
   );
 }
