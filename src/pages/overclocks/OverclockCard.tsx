658ee90185de93b9a777abfaffa4b9b32c3405f7
Robert Lynch <robertlynch2012@gmail.com>
2021-02-15 21:30:53 -0600

    Only do Overclock popover on info circle

--- src/pages/overclocks/OverclockCard.tsx
+++ src/pages/overclocks/OverclockCard.tsx
@@ -4,6 +4,7 @@ import { Card, Col, Popover } from "antd";
 import OverclockCardPopover from "./OverclockCardPopover";
 import { Miner, MinerColor } from "utils/miner";
 import { MinerWeapon } from "utils/weapons";
+import InfoCircleOutlined from "@ant-design/icons/InfoCircleOutlined";
 
 export default function OverclockCard(props: {
   overclock: Overclock;
@@ -13,37 +14,45 @@ export default function OverclockCard(props: {
   onClick: () => void;
 }) {
   return (
-    <Popover
-      placement="bottom"
-      content={() => (
-        <OverclockCardPopover
-          weapon={props.weapon}
-          overclock={props.overclock}
-        />
-      )}
-    >
-      <Col xxl={4} xl={6} lg={8} md={12} xs={24} key={props.overclock.name}>
-        <Card
-          hoverable
-          title={props.overclock.name}
-          headStyle={
-            props.isAcquired
-              ? {
-                  color: "black",
-                  backgroundColor: MinerColor[props.miner],
-                  fontWeight: "bold",
-                  transition: "all .2s",
-                }
-              : {
-                  fontWeight: "bold",
-                  transition: "all .2s",
-                }
-          }
-          onClick={props.onClick}
-        >
-          <OverclockIcon overclock={props.overclock} />
-        </Card>
-      </Col>
-    </Popover>
+    <Col xxl={4} xl={6} lg={8} md={12} xs={24} key={props.overclock.name}>
+      <Card
+        hoverable
+        title={props.overclock.name}
+        extra={
+          <Popover
+            // placement="bottom"
+            content={() => (
+              <OverclockCardPopover
+                weapon={props.weapon}
+                overclock={props.overclock}
+              />
+            )}
+          >
+            <InfoCircleOutlined
+              style={{
+                color: props.isAcquired ? "black" : "white",
+                transition: "all .2s",
+              }}
+            />
+          </Popover>
+        }
+        headStyle={
+          props.isAcquired
+            ? {
+                color: "black",
+                backgroundColor: MinerColor[props.miner],
+                fontWeight: "bold",
+                transition: "all .2s",
+              }
+            : {
+                fontWeight: "bold",
+                transition: "all .2s",
+              }
+        }
+        onClick={props.onClick}
+      >
+        <OverclockIcon overclock={props.overclock} />
+      </Card>
+    </Col>
   );
 }
