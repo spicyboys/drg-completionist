8cb3472d1355dfeb683a2fd6886c899dada704e9
Fleckeri <16787403+Fleckeri@users.noreply.github.com>
2021-02-19 13:35:57 -0600

    Add a few miscellaneous icons

--- src/assets/other/index.ts
+++ src/assets/other/index.ts
@@ -3,8 +3,11 @@
  * To update it, run `yarn run update-codegen`
  */
 
+export { default as AccessoryIcon } from './accessoryIcon.png';
 export { default as Assignment } from './assignment.png';
 export { default as ForgeMastery } from './forgeMastery.png';
+export { default as LostPack } from './lostPack.png';
 export { default as PerkStar } from './perkStar.png';
+export { default as PlayerRankIcon } from './playerRankIcon.png';
 export { default as RockAndStone } from './rockAndStone.png';
 export { default as SatchelCharge } from './satchelCharge.png';
