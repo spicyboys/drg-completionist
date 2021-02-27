42a7de0d4e690da884555a81a5d7bb06515a20b6
Fleckeri <16787403+Fleckeri@users.noreply.github.com>
2021-02-27 05:34:24 -0600

    Coffee button now opens in a new tab

--- src/components/settingsModal/BuyMeACoffee.tsx
+++ src/components/settingsModal/BuyMeACoffee.tsx
@@ -1,7 +1,11 @@
 export default function BuyMeACoffee() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <a href="https://www.buymeacoffee.com/robort">
+      <a
+        href="https://www.buymeacoffee.com/robort"
+        rel="noreferrer noopener"
+        target="_blank"
+      >
         <img
           src="https://img.buymeacoffee.com/button-api/?text=Buy me a Glyphid Slammer&emoji=🍺&slug=robort&button_colour=FFDD00&font_colour=111111&font_family=Poppins&outline_colour=000000&coffee_colour=FFDD00"
           alt="Coffee is for pointy-eared leaf-lovers."
