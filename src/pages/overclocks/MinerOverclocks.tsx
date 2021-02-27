2fa2c872b1c7b28678edbb8586ca263bfd105985
Robert Lynch <robertlynch2012@gmail.com>
2021-02-26 22:25:57 -0600

    useCallback in MinerOverclocks

--- src/pages/overclocks/MinerOverclocks.tsx
+++ src/pages/overclocks/MinerOverclocks.tsx
@@ -1,5 +1,5 @@
 import { Row } from 'antd';
-import React from 'react';
+import React, { useCallback } from 'react';
 import WeaponDivider from 'components/WeaponDivider';
 import { Overclock, Overclocks } from 'data/overclocks';
 import useStore from 'store/useStore';
@@ -15,55 +15,64 @@ export default function MinerOverclocks(props: { miner: Miner }) {
   ] = useStore();
   const minerOverclocks = Overclocks[miner];
 
-  const isAcquired = (weapon: MinerWeapon<Miner>, overclock: Overclock) => {
-    return acquiredOverclocks.get(weapon)?.has(overclock.name) ?? false;
-  };
+  const isAcquired = useCallback(
+    (weapon: MinerWeapon<Miner>, overclock: Overclock) => {
+      return acquiredOverclocks.get(weapon)?.has(overclock.name) ?? false;
+    },
+    [acquiredOverclocks]
+  );
 
-  const isForged = (weapon: MinerWeapon<Miner>, overclock: Overclock) => {
-    return forgedOverclocks.get(weapon)?.has(overclock.name) ?? false;
-  };
-
-  const toggleAcquired = (weapon: MinerWeapon<Miner>, overclock: Overclock) =>
-    dispatch({
-      type: 'TOGGLE_OVERCLOCK_ACQUIRED', // Placeholder value copied from ToggleForged()
-      payload: {
-        weapon: weapon,
-        overclock: overclock.name,
-      },
-    });
-
-  const toggleForged = (weapon: MinerWeapon<Miner>, overclock: Overclock) =>
-    dispatch({
-      type: 'TOGGLE_OVERCLOCK_FORGED',
-      payload: {
-        weapon: weapon,
-        overclock: overclock.name,
-      },
-    });
+  const isForged = useCallback(
+    (weapon: MinerWeapon<Miner>, overclock: Overclock) => {
+      return forgedOverclocks.get(weapon)?.has(overclock.name) ?? false;
+    },
+    [forgedOverclocks]
+  );
 
   // Cycles a given overclock state from unacquired, to acquired-but-unforged, to forged, then back to unacquired
-  const setOverclockState = (
-    weapon: MinerWeapon<Miner>,
-    overclock: Overclock
-  ) => {
-    // Set from unacquired to acquired-but-unforged
-    if (!isAcquired(weapon, overclock) && !isForged(weapon, overclock)) {
-      toggleAcquired(weapon, overclock);
-    }
-    // Set from acquired-but-unforged to forged
-    else if (isAcquired(weapon, overclock) && !isForged(weapon, overclock)) {
-      toggleForged(weapon, overclock);
-    }
-    // Set from forged back to unacquired
-    else if (isForged(weapon, overclock)) {
-      toggleAcquired(weapon, overclock);
-      toggleForged(weapon, overclock);
-    }
-    // Set from unintentional isForged && !isAcquired edge case to unacquired
-    else {
-      toggleForged(weapon, overclock);
-    }
-  };
+  const setOverclockState = useCallback(
+    (weapon: MinerWeapon<Miner>, overclock: Overclock) => {
+      const toggleAcquired = (
+        weapon: MinerWeapon<Miner>,
+        overclock: Overclock
+      ) =>
+        dispatch({
+          type: 'TOGGLE_OVERCLOCK_ACQUIRED',
+          payload: {
+            weapon: weapon,
+            overclock: overclock.name,
+          },
+        });
+
+      const toggleForged = (weapon: MinerWeapon<Miner>, overclock: Overclock) =>
+        dispatch({
+          type: 'TOGGLE_OVERCLOCK_FORGED',
+          payload: {
+            weapon: weapon,
+            overclock: overclock.name,
+          },
+        });
+
+      // Set from unacquired to acquired-but-unforged
+      if (!isAcquired(weapon, overclock) && !isForged(weapon, overclock)) {
+        toggleAcquired(weapon, overclock);
+      }
+      // Set from acquired-but-unforged to forged
+      else if (isAcquired(weapon, overclock) && !isForged(weapon, overclock)) {
+        toggleForged(weapon, overclock);
+      }
+      // Set from forged back to unacquired
+      else if (isForged(weapon, overclock)) {
+        toggleAcquired(weapon, overclock);
+        toggleForged(weapon, overclock);
+      }
+      // Set from unintentional isForged && !isAcquired edge case to unacquired
+      else {
+        toggleForged(weapon, overclock);
+      }
+    },
+    [dispatch, isAcquired, isForged]
+  );
 
   return (
     <>
