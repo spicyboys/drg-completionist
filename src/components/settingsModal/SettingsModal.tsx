2b9b44bd01414631645e30691f00805dd8719243
Fleckeri <16787403+Fleckeri@users.noreply.github.com>
2021-02-27 05:34:58 -0600

    Add GitHub latest version badge to Settings modal

--- src/components/settingsModal/SettingsModal.tsx
+++ src/components/settingsModal/SettingsModal.tsx
@@ -35,6 +35,18 @@ export default function SettingsModal(props: {
       <Divider dashed />
       <Row justify="center">
         <Col span={18} style={{ textAlign: 'center' }}>
+          <a
+            href="https://github.com/BobertForever/drg-completionist/releases/"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            <img
+              alt="Latest GitHub release (latest SemVer)"
+              src="https://img.shields.io/github/v/release/BobertForever/drg-completionist"
+              style={{ marginBottom: 12, marginTop: -6 }}
+            />
+          </a>
+          <br />
           Up-to-date as of Deep Rock Galactic version{' '}
           <Text code>1.33.49660.0</Text>
         </Col>
