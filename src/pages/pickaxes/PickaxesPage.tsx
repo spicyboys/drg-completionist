e73e3f951d6da8207e9220206a543bf6c8b5c6e7
Fleckeri <16787403+Fleckeri@users.noreply.github.com>
2021-03-10 01:13:38 -0600

    Change how Ant icon is imported

--- src/pages/pickaxes/PickaxesPage.tsx
+++ src/pages/pickaxes/PickaxesPage.tsx
@@ -1,4 +1,4 @@
-import RightOutlined from '@ant-design/icons/RightOutlined';
+import { RightOutlined } from '@ant-design/icons';
 import { Collapse } from 'antd';
 import { useCallback } from 'react';
 import { Pickaxes, PickaxeSets } from 'data/pickaxes';
