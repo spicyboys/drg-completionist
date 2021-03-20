237411956cfd3c709ec963419ef770c4b0fe841b
Fleckeri <16787403+Fleckeri@users.noreply.github.com>
2021-03-20 03:02:34 -0500

    Add an optional allClass prop to render All-Class section

--- src/pages/MinerPageLayout.tsx
+++ src/pages/MinerPageLayout.tsx
@@ -1,15 +1,28 @@
 import { RightOutlined } from '@ant-design/icons';
 import { Collapse } from 'antd';
-import { AllMiners, Miner } from 'utils/miner';
+import {
+  AllMiners,
+  AllMinersWithAllClass,
+  Miner,
+  MinerWithAllClass,
+} from 'utils/miner';
 import MinerCard from './MinerCard';
 import { ProgressQuery } from './MinerCardProgressBar';
 
+/**
+ * Returns a Collapse with a card for each Miner containing an Avatar
+ * and Progress Bar. Will also include an All-Class card if `allClass`
+ * prop is set to true.
+ * @param props
+ * @returns JSX.Element
+ */
 export default function MinerPageLayout(props: {
+  allClass?: boolean;
   category: string;
-  children: (miner: Miner) => React.ReactNode;
+  children: (miner: Miner | MinerWithAllClass) => React.ReactNode;
   getProgress: ProgressQuery;
 }) {
-  const { category } = props;
+  const { allClass, category } = props;
 
   return (
     <Collapse
@@ -23,16 +36,27 @@ export default function MinerPageLayout(props: {
         />
       )}
     >
-      {AllMiners.map((miner) => (
-        <MinerCard
-          key={miner}
-          category={category}
-          miner={miner}
-          getProgress={props.getProgress}
-        >
-          {props.children}
-        </MinerCard>
-      ))}
+      {allClass
+        ? AllMinersWithAllClass.map((miner) => (
+            <MinerCard
+              key={miner}
+              category={category}
+              miner={miner}
+              getProgress={props.getProgress}
+            >
+              {props.children}
+            </MinerCard>
+          ))
+        : AllMiners.map((miner) => (
+            <MinerCard
+              key={miner}
+              category={category}
+              miner={miner}
+              getProgress={props.getProgress}
+            >
+              {props.children}
+            </MinerCard>
+          ))}
     </Collapse>
   );
 }
