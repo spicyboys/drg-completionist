37429066957ee3127242918df9b43220865f5ca5
Fleckeri <16787403+Fleckeri@users.noreply.github.com>
2021-03-20 02:56:25 -0500

    Add armorPaintjobs table to IndexedDB

--- src/db/AppDatabase.ts
+++ src/db/AppDatabase.ts
@@ -1,11 +1,12 @@
 import { Dexie } from 'dexie';
+import { ArmorPaintjobNames } from 'data/armor';
 import { Framework } from 'data/frameworks';
 import {
   PickaxeSets,
   PickaxeParts,
   PickaxeUniquePartNames,
 } from 'data/pickaxes';
-import { Miner } from 'utils/miner';
+import { Miner, MinerWithAllClass } from 'utils/miner';
 import { MinerWeapon } from 'utils/weapons';
 
 export class AppDatabase extends Dexie {
@@ -13,20 +14,23 @@ export class AppDatabase extends Dexie {
   frameworks: Dexie.Table<FrameworkEntry, number>;
   pickaxes: Dexie.Table<PickaxeEntry, number>;
   pickaxeUniques: Dexie.Table<PickaxeUniquePartEntry, number>;
+  armorPaintjobs: Dexie.Table<ArmorPaintjobEntry, number>;
 
   constructor() {
     super('DRG-Completionist');
-    this.version(2).stores({
+    this.version(3).stores({
       overclocks: '[weapon+name], weapon',
       frameworks: '[weapon+name], weapon',
       pickaxes: '[part+name], name',
       pickaxeUniques: 'name',
+      armorPaintjobs: '[miner+name], name',
     });
 
     this.overclocks = this.table('overclocks');
     this.frameworks = this.table('frameworks');
     this.pickaxes = this.table('pickaxes');
     this.pickaxeUniques = this.table('pickaxeUniques');
+    this.armorPaintjobs = this.table('armorPaintjobs');
   }
 
   /** Async call to clear all current IndexedDB tables completely. */
@@ -48,4 +52,9 @@ export type PickaxeEntry = {
 
 export type PickaxeUniquePartEntry = {
   name: typeof PickaxeUniquePartNames[number];
+};
+
+export type ArmorPaintjobEntry = {
+  miner: MinerWithAllClass;
+  name: typeof ArmorPaintjobNames[MinerWithAllClass][number];
 };
