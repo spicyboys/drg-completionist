263d519fb5ceadcd178ccbfd11245c8372c3573c
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-01-04 06:56:32 +0100

    Make framework page only show valid framworks
    Since weapons do not have to have an existing framework, we have to
    check if a weapon has a framework before displaying it.

--- src/pages/frameworks/MinerFrameworks.tsx
+++ src/pages/frameworks/MinerFrameworks.tsx
@@ -1,7 +1,7 @@
 import { Row } from 'antd';
 import React from 'react';
 import WeaponDivider from 'components/WeaponDivider';
-import { Frameworks } from 'data/frameworks';
+import { FrameworkIDs, Frameworks } from 'data/frameworks';
 import { Miner } from 'data/miner';
 import { MinerWeapon, MinerWeapons } from 'data/weapons';
 import FrameworkCard from './FrameworkCard';
@@ -15,13 +15,14 @@ export default function MinerFrameworks<T extends Miner>(props: { miner: T }) {
         <React.Fragment key={weapon}>
           <WeaponDivider weapon={weapon} />
           <Row gutter={[16, 16]}>
-            {Frameworks.map((framework) => (
-              <FrameworkCard
-                key={framework}
-                miner={miner}
-                weapon={weapon}
-                framework={framework}
-              />
+            {Frameworks.filter((framework) => FrameworkIDs[weapon][framework] !== undefined)
+              .map((framework) => (
+                <FrameworkCard
+                  key={framework}
+                  miner={miner}
+                  weapon={weapon}
+                  framework={framework}
+                />
             ))}
           </Row>
         </React.Fragment>
