9e89b39b82848edff6375e613a59b0b98c4a8d80
Jonas Bergmann <bergmann.jonas31@gmail.com>
2023-01-09 04:04:00 +0100

    Make victory pose image popup start at bottom of card

--- src/pages/victoryPoses/VictoryPoseIcon.tsx
+++ src/pages/victoryPoses/VictoryPoseIcon.tsx
@@ -7,41 +7,41 @@ export default function VictoryPoseIcon(props: {
   victoryPose: VictoryPose | CommonVictoryPose;
 }) {
   return (
-    <div
-      style={{
-        position: 'relative',
-        height: 100,
-        width: 100,
-        margin: 'auto',
-      }}
+    <Popover
+      trigger={isMobile ? 'click' : 'hover'}
+      destroyTooltipOnHide
+      placement="bottom"
+      content={() => (
+        <Image
+          alt={props.victoryPose.name}
+          src={props.victoryPose.icon}
+          style={{ height: 400, width: 'auto' }}
+        />
+      )}
     >
       <div
         style={{
-          position: 'absolute',
-          transform: 'translate(-50%,-50%)',
-          top: '50%',
-          left: '50%',
+          position: 'relative',
+          height: 100,
+          width: 100,
+          margin: 'auto',
         }}
       >
-        <Popover
-          trigger={isMobile ? 'click' : 'hover'}
-          destroyTooltipOnHide
-          placement='bottom'
-          content={() => (
-              <Image
-                alt={props.victoryPose.name}
-                src={props.victoryPose.icon}
-                style={{ height: 380, width: 'auto' }}
-              />
-          )}
+        <div
+          style={{
+            position: 'absolute',
+            transform: 'translate(-50%,-50%)',
+            top: '50%',
+            left: '50%',
+          }}
         >
           <Image
             alt={props.victoryPose.name}
             src={props.victoryPose.icon}
             style={{ height: 100, width: 'auto' }}
           />
-        </Popover>
+        </div>
       </div>
-    </div>
+    </Popover>
   );
 }
