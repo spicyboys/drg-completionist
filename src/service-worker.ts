273aa7e837f906109e9dd00f02142cbe92e43ebd
Nich Osborn <admin@nich.codes>
2023-07-10 17:37:51 -0500

    Increase cache's max entries

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -72,7 +72,7 @@ registerRoute(
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
+      new ExpirationPlugin({ maxEntries: 10000 }),
     ],
   })
 );
