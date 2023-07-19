07daddcd9a17e47953b7027b77ad3368b785275b
Robert Lynch <robertlynch2012@gmail.com>
2023-07-18 21:13:47 -0700

    Add overclock types

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -5,8 +5,11 @@ export async function createSchemaCustomization({
 }: CreateSchemaCustomizationArgs): Promise<void> {
   const typeDefs = `
     type MinersJson implements Node {
-      name: String!
       weapons: [WeaponsJson]! @link(by: "name")
+    }
+
+    type WeaponsJsonOverclocks implements Node {
+      type: OverclockTypesJson! @link(by: "name")
     }
   `;
   createTypes(typeDefs);
