5d682cb76f77e973ee7e1b78ef15e9fc17df7e07
Robert Lynch <robertlynch2012@gmail.com>
2023-07-29 19:03:35 -0700

    Get weapon paint jobs from the save file

---
+++ src/hooks/analyze/useGetWeaponPaintJobsFromSaveFile.ts
@@ -0,0 +1,92 @@
+import { graphql, useStaticQuery } from "gatsby";
+import { type WeaponPaintJobEntry } from "../db/AppDatabase";
+import { useCallback } from "react";
+import { type SaveFile } from "./useParseSaveFile";
+
+/**
+ * Get the weapon paint jobs from a save file.
+ *
+ * Weapon paint jobs are unlocked in two ways:
+ *   1. By acquiring & forging a schematic for the paint job (stored as a
+ *      matrix core ID, which is unique for each miner)
+ *   2. By unlocking the paint job directly (stored as the weapon paint job's
+ *      ID, which is unique for each paint job, not for each miner)
+ *
+ * For both cases, paint jobs are unlocked at a miner level, not for an
+ * individual weapon. Not all paint jobs all acquirable by a schematic, so not
+ * all paint jobs have a corresponding matrix core ID, but all paint jobs have
+ * a corresponding unique weapon paint job ID.
+ */
+export default function useGetWeaponPaintJobsFromSaveFile(): (
+  saveFile: SaveFile
+) => WeaponPaintJobEntry[] {
+  const {
+    allMinersJson: { nodes: miners },
+  } = useStaticQuery<Queries.GetWeaponPaintJobsFromSaveFileQuery>(graphql`
+    query GetWeaponPaintJobsFromSaveFile {
+      allMinersJson {
+        nodes {
+          name
+          weaponPaintJobs {
+            weaponPaintJob {
+              name
+              saveId
+            }
+            saveId
+          }
+          weapons {
+            saveId
+          }
+        }
+      }
+    }
+  `);
+
+  return useCallback(
+    ({
+      SchematicSave: {
+        SchematicSave: {
+          OwnedSchematics: unforgedSchematics,
+          ForgedSchematics: forgedSchematics,
+        },
+      },
+      UnlockedItemSkins: unlockedItemSkins,
+    }: SaveFile) => {
+      const acquiredWeaponPaintJobs: WeaponPaintJobEntry[] = [];
+      for (const miner of miners) {
+        for (const weaponPaintJob of miner.weaponPaintJobs) {
+          if (unforgedSchematics?.includes(weaponPaintJob.saveId)) {
+            acquiredWeaponPaintJobs.push({
+              miner: miner.name,
+              name: weaponPaintJob.weaponPaintJob.name,
+              isForged: false,
+            });
+          } else if (forgedSchematics?.includes(weaponPaintJob.saveId)) {
+            acquiredWeaponPaintJobs.push({
+              miner: miner.name,
+              name: weaponPaintJob.weaponPaintJob.name,
+              isForged: true,
+            });
+          } else if (
+            // Weapon paint jobs are unlocked at a miner level, not for an
+            // individual weapon, but when a paint job is unlocked, it's
+            // unlocked for all weapons for that miner, so we just check the
+            // first weapon.
+            unlockedItemSkins[miner.weapons[0].saveId].Skins.includes(
+              weaponPaintJob.weaponPaintJob.saveId
+            )
+          ) {
+            acquiredWeaponPaintJobs.push({
+              miner: miner.name,
+              name: weaponPaintJob.weaponPaintJob.name,
+              isForged: true,
+            });
+          }
+        }
+      }
+
+      return acquiredWeaponPaintJobs;
+    },
+    [miners]
+  );
+}
--- src/hooks/analyze/useParseSaveFile.ts
+++ src/hooks/analyze/useParseSaveFile.ts
@@ -4,6 +4,7 @@ import useGetOverclocksFromSaveFile from "./useGetOverclocksFromSaveFile";
 import init, { parse_save_file } from "drg-save-parser";
 import { useDB } from "../db";
 import { z } from "zod";
+import useGetWeaponPaintJobsFromSaveFile from "./useGetWeaponPaintJobsFromSaveFile";
 
 // Wasm-bindgen doesn't support Vec<String>, so we have to throw the whole
 // object back untyped. Since working with untyped data is error-prone, we
@@ -48,20 +49,25 @@ export type SaveFile = z.infer<typeof SaveFileSchema>["properties"];
 export default function useParseSaveFile() {
   const db = useDB();
   const getOverclocksFromSaveFile = useGetOverclocksFromSaveFile();
+  const getWeaponPaintJobsFromSaveFile = useGetWeaponPaintJobsFromSaveFile();
   return useCallback(
     async (f: RcFile): Promise<void> => {
       try {
         // Parse the save file using the WASM library
         await init();
-        const saveFile = SaveFileSchema.parse(await parse_save_file(f));
+        const saveFile = SaveFileSchema.parse(
+          await parse_save_file(f)
+        ).properties;
 
         // Extract the relevant information from the parsed save file
-        const overclocks = getOverclocksFromSaveFile(saveFile.properties);
+        const overclocks = getOverclocksFromSaveFile(saveFile);
+        const weaponPaintJobs = getWeaponPaintJobsFromSaveFile(saveFile);
 
         // Update the store with the new save file data
         await db.transaction("rw", db.tables, async () => {
           await db.clearAll();
           await db.overclocks.bulkAdd(overclocks);
+          await db.weaponPaintjobs.bulkAdd(weaponPaintJobs);
         });
       } catch (e) {
         console.error(e);
