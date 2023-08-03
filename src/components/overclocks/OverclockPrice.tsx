ff6e4a5623ab00826f99ccefc602c60d7a30cae5
NichCodes <admin@nich.codes>
2023-08-02 22:23:11 -0500

    Fix black on gray text

--- src/components/overclocks/OverclockPrice.tsx
+++ src/components/overclocks/OverclockPrice.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { Col, Row, Space, Tooltip } from "antd";
-import Text from "antd/lib/typography/Text";
-import { graphql } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import React from 'react';
+import { Col, Row, Space, Tooltip } from 'antd';
+import Text from 'antd/lib/typography/Text';
+import { graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function OverclockPrice({
   overclock: { price },
@@ -10,102 +10,116 @@ export default function OverclockPrice({
   overclock: Queries.OverclockPriceOverclockFragment;
 }) {
   return (
-    <Row align="middle" justify="space-around">
+    <Row align='middle' justify='space-around'>
       {price?.credits && (
         <Col>
           <Space>
-            <Tooltip placement="top" title="Credits">
+            <Tooltip placement='top' title='Credits'>
               <StaticImage
-                src="../../images/currencies/credit.png"
-                alt="Credits"
+                src='../../images/currencies/credit.png'
+                alt='Credits'
                 height={20}
               />
             </Tooltip>
-            <Text strong>{price.credits}</Text>
+            <Text strong style={{ color: 'rgba(255, 255, 255, 0.85)' }}>
+              {price.credits}
+            </Text>
           </Space>
         </Col>
       )}
       {price?.croppa && (
         <Col>
           <Space>
-            <Tooltip placement="top" title="Croppa">
+            <Tooltip placement='top' title='Croppa'>
               <StaticImage
-                src="../../images/currencies/croppa.png"
-                alt="Croppa"
+                src='../../images/currencies/croppa.png'
+                alt='Croppa'
                 height={20}
               />
             </Tooltip>
-            <Text strong>{price.croppa}</Text>
+            <Text strong style={{ color: 'rgba(255, 255, 255, 0.85)' }}>
+              {price.croppa}
+            </Text>
           </Space>
         </Col>
       )}
       {price?.enorPearl && (
         <Col>
           <Space>
-            <Tooltip placement="top" title="Enor Pearls">
+            <Tooltip placement='top' title='Enor Pearls'>
               <StaticImage
-                src="../../images/currencies/enor-pearl.png"
-                alt="Enor Pearls"
+                src='../../images/currencies/enor-pearl.png'
+                alt='Enor Pearls'
                 height={20}
               />
             </Tooltip>
-            <Text strong>{price.enorPearl}</Text>
+            <Text strong style={{ color: 'rgba(255, 255, 255, 0.85)' }}>
+              {price.enorPearl}
+            </Text>
           </Space>
         </Col>
       )}
       {price?.magnite && (
         <Col>
           <Space>
-            <Tooltip placement="top" title="Magnite">
+            <Tooltip placement='top' title='Magnite'>
               <StaticImage
-                src="../../images/currencies/magnite.png"
-                alt="Magnite"
+                src='../../images/currencies/magnite.png'
+                alt='Magnite'
                 height={20}
               />
             </Tooltip>
-            <Text strong>{price.magnite}</Text>
+            <Text strong style={{ color: 'rgba(255, 255, 255, 0.85)' }}>
+              {price.magnite}
+            </Text>
           </Space>
         </Col>
       )}
       {price?.jadiz && (
         <Col>
           <Space>
-            <Tooltip placement="top" title="Jadiz">
+            <Tooltip placement='top' title='Jadiz'>
               <StaticImage
-                src="../../images/currencies/jadiz.png"
-                alt="Jadiz"
+                src='../../images/currencies/jadiz.png'
+                alt='Jadiz'
                 height={20}
               />
             </Tooltip>
-            <Text strong>{price.jadiz}</Text>
+            <Text strong style={{ color: 'rgba(255, 255, 255, 0.85)' }}>
+              {price.jadiz}
+            </Text>
           </Space>
         </Col>
       )}
       {price?.umanite && (
         <Col>
           <Space>
-            <Tooltip placement="top" title="Umanite">
+            <Tooltip placement='top' title='Umanite'>
               <StaticImage
-                src="../../images/currencies/umanite.png"
-                alt="Umanite"
+                src='../../images/currencies/umanite.png'
+                alt='Umanite'
                 height={20}
               />
             </Tooltip>
-            <Text strong>{price.umanite}</Text>
+            <Text strong style={{ color: 'rgba(255, 255, 255, 0.85)' }}>
+              {price.umanite}
+            </Text>
           </Space>
         </Col>
       )}
       {price?.bismor && (
         <Col>
           <Space>
-            <Tooltip placement="top" title="Bismor">
+            <Tooltip placement='top' title='Bismor'>
               <StaticImage
-                src="../../images/currencies/bismor.png"
-                alt="Bismor"
+                src='../../images/currencies/bismor.png'
+                alt='Bismor'
                 height={20}
               />
             </Tooltip>
-            <Text strong>{price.bismor}</Text>
+            <Text strong style={{ color: 'rgba(255, 255, 255, 0.85)' }}>
+              {price.bismor}
+            </Text>
           </Space>
         </Col>
       )}
